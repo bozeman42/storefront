67531d409f4a2b7628873580c6ac2db0533cbe36
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-04-20 00:44:56 -0500

    added example.env file. added database connection

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,12 +2,4 @@ const { Pool, Client } = require('pg')
 
 const pool = new Pool()
 
-const getNow = async () => {
-  const res = await pool.query('SELECT NOW()')
-  console.log(res)
-  await pool.end()
-}
-
-getNow()
-
 module.exports = pool
