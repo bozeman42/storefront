a35c18e6b65dc6b9b8d68b56d3a8cb9c5200b20c
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-08-24 17:56:04 -0500

    category service call no longer returns duplicates of categories used more than once

--- server/routers/router-category.js
+++ server/routers/router-category.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool.js')
 router.get('/', (req, res) => {
   pool.connect().then(client => {
     const categoryQuery = `
-    SELECT category_id as id, name as category FROM
+    SELECT DISTINCT category_id as id, name as category FROM
     categories RIGHT JOIN items_categories USING (category_id);`
     client
       .query(categoryQuery)
