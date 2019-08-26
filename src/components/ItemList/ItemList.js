895862cb16e9ab8d122b1839b8f09bef9da073d6
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-08-25 19:11:42 -0500

    server now builds category array with category ID server side using ItemModel

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -5,19 +5,34 @@ const renderQuantity = quantity => {
   return <p>{quantity} in stock</p>
 }
 
-const Item = ({id, name, description, price, quantity}) => (
+const Item = ({ id, name, description, price, quantity, categories }) => (
   <div className='store-item'>
-    <img className='item-image' alt='Phil Murray' src='https://www.fillmurray.com/150/150' />
+    <img
+      className='item-image'
+      alt='Phil Murray'
+      src='https://www.fillmurray.com/150/150'
+    />
     <p>{name}</p>
     <p>{description}</p>
-    <p><strong>${price / 100}</strong></p>
+    <p>
+      <strong>${price / 100}</strong>
+    </p>
     {renderQuantity(quantity)}
+    {renderCategories(categories)}
   </div>
 )
 
-const ItemList = ({items}) => (
+const renderCategories = categories => {
+  return categories.map(({ id, category }) => {
+    return <p key={id}>{category}</p>
+  })
+}
+
+const ItemList = ({ items }) => (
   <div className='item-list'>
-    {items.map(item => <Item key={item.id} {...item} />)}
+    {items.map(item => (
+      <Item key={item.id} {...item} />
+    ))}
   </div>
 )
 
