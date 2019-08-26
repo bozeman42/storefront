9b7d8d62454521e973738786a9b0f05632519a74
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-08-25 19:20:14 -0500

    improved whitespace in addCategories

--- server/modules/addItem.js
+++ server/modules/addItem.js
@@ -35,14 +35,17 @@ const addItem = item => {
 const addCategories = async (categories, itemId, client) => {
   const categoryIds = await categories.reduce(async (previousPromise, category) => {
     const ids = await previousPromise
+
     let result = await client.query(`
       INSERT INTO categories (name) values ($1) ON CONFLICT (name) DO NOTHING RETURNING category_id ;
     `, [category])
+
     if (result.rows.length === 1) return [...ids, result.rows[0].category_id]
+
     result = await client.query(`
       SELECT category_id FROM categories WHERE name = $1;
     `, [category])
-    console.log(ids)
+
     return [...ids, result.rows[0].category_id]
   }, Promise.resolve([]))
   
